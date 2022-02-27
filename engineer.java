0899e75ef6809d88a2c3beee047353b75e8519e6
Gowri sree Anilkumar <85624436+gowri30@users.noreply.github.com>
2022-02-27 11:55:44 +0400

    Add files via upload

---
+++ engineer.java
@@ -0,0 +1,27 @@
+class Employee
+{
+    void display()
+    {
+        System.out.println("Name of class is Employee.");
+    }
+
+    void calcSalary()
+    {
+        System.out.println("Salary of employee is 10000");
+    }
+}
+
+public class engineer {
+    void calcSalary()
+    {
+        super.calcSalary();
+        System.out.println("Salary of engineer is 20000");
+    }
+
+    public static void main(String args[])
+    {
+        Engineer e=new Engineer();
+        e.display();
+        e.calcSalary();
+    }
+}
---
+++ garbage.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class garbage {
+    public static void main(String args[]){  
+        Student obj=new Student();  
+        obj.getDetails();
+        obj.showDetails();
+        obj=null;  
+        System.gc();  
+       
+       }
+}
+class Student
+{
+    Scanner sc = new Scanner(System.in);
+    String name,number;
+    int rollno;
+    
+    void getDetails()
+    {
+        System.out.printf("Enter the Name of Student : ");
+        name = sc.next();
+        System.out.printf("Enter the Roll No : ");
+        rollno = sc.nextInt();
+        System.out.printf("Enter Phone Number : ");
+        number = sc.next();
+
+    }
+
+    void showDetails()
+    {
+     System.out.printf("\nThe Name of Student is %s \n",name);
+     System.out.printf("Enter the Roll No : %d \n",rollno);
+     System.out.printf("Enter Phone Number : %s \n",number);
+    }
+
+    protected void finalize()
+    {
+        System.out.println("\nGarbage of Object Collected");
+    }
+}
